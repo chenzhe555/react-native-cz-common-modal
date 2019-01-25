f854d2ef33d11e9a3d198ee5721984907cba0a95
chenzhe <chenzhe@meicai.cn>
2019-01-25 21:38:23 +0800

    初始化项目

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+import React, { Component } from 'react';
+import { View, AppRegistry } from 'react-native';
+import CZPackElement from 'react-native-cz-pack-element';
 
-import { NativeModules } from 'react-native';
 
-const { RNCzCommonModal } = NativeModules;
+const modalsMap = {}
+//目前仅提供显示隐藏
+export default class CZCommonModal extends Component{
 
-export default RNCzCommonModal;
+    //显示Modal视图
+    static show = (view = null, key = 'defaultKey') => {
+        if (view) {
+            //如果已存在Key对应的，直接移除
+            if(modalsMap.hasOwnProperty(key)) {
+                let element = modalsMap[key];
+                if (element) element.destoryElement();
+            }
+            let element = new CZPackElement(view);
+            modalsMap[key] = element;
+        }
+    };
+
+    //隐藏Modal视图
+    static hide = (key = 'defaultKey') => {
+        let element = modalsMap[key];
+        if (element) element.destoryElement();
+    }
+}
