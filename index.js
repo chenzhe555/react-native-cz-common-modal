9dc4dd447438a7b3c6511414147b939c04f1c6da
chenzhe <chenzhe@meicai.cn>
2019-01-25 21:42:22 +0800

    nothing

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ import CZPackElement from 'react-native-cz-pack-element';
 const modalsMap = {}
 //目前仅提供显示隐藏
 export default class CZCommonModal extends Component{
-
-    //显示Modal视图
+    //显示Modal视图,获取引用可以，不建议自己移除组件，需调用hide方法
     static show = (view = null, key = 'defaultKey') => {
+        let element = null;
         if (view) {
             //如果已存在Key对应的，直接移除
             if(modalsMap.hasOwnProperty(key)) {
-                let element = modalsMap[key];
-                if (element) element.destoryElement();
+                element = modalsMap[key];
+                element.destoryElement();
             }
-            let element = new CZPackElement(view);
+            element = new CZPackElement(view);
             modalsMap[key] = element;
         }
+        return element;
     };
 
     //隐藏Modal视图
